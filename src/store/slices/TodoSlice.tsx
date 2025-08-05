2a89f648eee0e37b5fca3a86b15175b79085a4bc
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-08-05 16:55:14 +0500

    Redux Work Started

---
+++ src/store/slices/TodoSlice.tsx
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const TodoSlice = createSlice ({
+  name: "todo",
+  initialState: [],
+  reducers:{
+    addTodo(state,action){
+
+    },
+    
+    deleteTodo(state,action){
+
+    },
+
+  }
+})
+
+export default TodoSlice
